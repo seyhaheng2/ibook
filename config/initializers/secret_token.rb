e9db30962c665728911dab076f9298c943235448
seyhaheng2 <seyhaheng2@gmail.com>
2013-12-03 15:21:54 +0700

    railskhmer.us

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ibook::Application.config.secret_key_base = '2e4cc5051042fb4dd698316b1187b1b2a1279404559dd1a86d13889cd5272888be694dcc33b222c93b84938fc11c2a7c1ecf45c047cc966d55736ff6a9a75cd3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ibook::Application.config.session_store :cookie_store, key: '_ibook_session'
